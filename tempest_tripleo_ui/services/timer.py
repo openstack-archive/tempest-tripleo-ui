901d1c9ab67fb53f2b916f4954a53ca4869369fe
Udi Kalifon <ukalifon@redhat.com>
2018-09-26 15:10:28 +0200

    Selenium infra
    Add Selenium to the tempest plugin
    
    Change-Id: I53207b321e8dabda1e47c4d9f01cdd6e1682bb69

---
+++ tempest_tripleo_ui/services/timer.py
@@ -0,0 +1,40 @@
+from time import time
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Timer(object):
+
+    def __init__(self, operation_name, timeout=10):
+        self.timer = time()
+        self.operationName = operation_name
+        self.timeout = timeout
+        self.duration = None
+
+    def assert_timer(self, message=None):
+        if self.timer is None:
+            logger.error("trying to use the same timer twice - please "
+                            "create a new timer to start a new measurement")
+            return
+
+        self.duration = time() - self.timer
+        self.timer = None
+
+        if self.duration > self.timeout:
+            errMsg = ("operation '{}' took {} seconds, allowed timeout "
+                      "is {} seconds").format(self.operationName,
+                                              self.duration,
+                                              self.timeout)
+            if message is not None:
+                errMsg += ": {}".format(message)
+            logger.error(errMsg)
+            return False
+
+        return True
+
+    def getDuration(self):
+        if self.duration is not None:
+            return self.duration
+
+        return time() - self.timer
