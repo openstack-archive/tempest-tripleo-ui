2adfcbaf3b2bbc021a0886d613446e3b5bb94422
Udi Kalifon <ukalifon@redhat.com>
2018-09-26 15:10:28 +0200

    Selenium infra
    Add Selenium to the tempest plugin
    
    Change-Id: I53207b321e8dabda1e47c4d9f01cdd6e1682bb69

---
+++ tempest_tripleo_ui/decorators.py
@@ -0,0 +1,48 @@
+from functools import wraps
+from selenium.common.exceptions import (
+    StaleElementReferenceException, InvalidElementStateException
+)
+import logging
+
+logger = logging.getLogger(__name__)
+
+def selenium_action(f):
+    """The selenium_action decorator
+
+    This decorator can be used to wrap methods of ``SeleniumElement`` to
+    provide exception handling, and automatic retries.
+
+    It expects that the method being decorated accepts a ``webelement``
+    as its first positional argument after ``self``.
+
+    E.g.
+
+        @selenium_action
+        def click_button(self, webelement):
+            ...
+    """
+
+    @wraps(f)
+    def wrapper(self, *args, **kwargs):
+        webelement = self.get_element()
+
+        if not webelement:
+            return
+
+        try:
+            try:
+                return f(self, webelement, *args, **kwargs)
+            except (StaleElementReferenceException,
+                    InvalidElementStateException):
+                self.wait_for_element(3)
+                webelement = self.get_element()
+                if webelement is None:
+                    return None
+                return f(self, webelement, *args, **kwargs)
+        except Exception as e:
+            logger.warning("selenium exception on '{}': {}"
+                              .format(self.get_name(), e))
+            raise
+        return None
+
+    return wrapper
